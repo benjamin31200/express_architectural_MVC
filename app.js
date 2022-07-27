a3fce54c8fd614697f8e9a40e8eecd75eb510071
benjamin31200 <benjamindeneux@hotmail.com>
2022-07-27 20:12:56 +0200

    hash du MDP utilisateur en BDD

--- app.js
+++ app.js
@@ -1,23 +1,13 @@
-const express = require("express");
-const moviesRouter = require("./routes/movies");
-const usersRouter = require("./routes/users");
+const { setupRoutes } = require('./routes');
+const express = require('express');
 const app = express();
 
-const port = 5000;
+const port = process.env.PORT || 3000;
 
-const welcome = (req, res) => {
-  res.send("Welcome to my favourite movie list");
-};
+app.use(express.json());
 
-app.get("/", welcome);
+setupRoutes(app);
 
-app.use("/api/movies", moviesRouter);
-app.use("/api/users", usersRouter);
-
-app.listen(port, (err) => {
-  if (err) {
-    console.error("Something bad happened");
-  } else {
-    console.log(`Server is listening on ${port}`);
-  }
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
 });
